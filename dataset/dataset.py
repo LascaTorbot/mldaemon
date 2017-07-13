b4ad1c1049faff36947fb731bbeca4a44c9577b4
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2017-07-12 21:41:05 -0300

    added dataset class

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,63 +1,89 @@
-import utils
+from mldaemon.utils.output import *
+from mldaemon.utils.pkldata import *
+from pymongo import MongoClient
+import numpy as np
+import os
 
-data = utils.load_data('../analysis.pkl')
-pe_sections = utils.load_data('../dicts/pe_sections.pkl')
-pe_imports_dict = utils.load_data('../dicts/pe_imports_dll.pkl')
+MIN_POSITIVES_RATE = 0.51
 
-# put the keys in order
-pe_imports = []
-for k in pe_imports_dict:
-    pe_imports.append((k, pe_imports_dict[k]))
+class Dataset:
+    def __init__(self, mongo_host, mongo_port, dict_path):
+        self.client = MongoClient(mongo_host, mongo_port)
+        self.db = self.client.cuckoo
 
-dataset = []
-count = 0
-limit = len(data)
+        # loading dictionary files
+        self.pe_sections = load_data(os.path.join(dict_path, 'pe_section.pkl'))
+        self.pe_imports_dict = load_data(os.path.join(dict_path, 'pe_imports_dict.pkl'))
 
-for obj in data:
-    count += 1
-    utils.progress(count, limit)
+        # put the keys in order
+        self.pe_imports = []
+        for k in pe_imports_dict:
+            self.pe_imports.append((k, pe_imports_dict[k]))
 
-    X = []
-    if 'static' in obj and 'pe_sections' in obj['static']\
-            and 'pe_imports' in obj['static']:
+    def get_dataset(self):
+        """
+        This func returns a tuple with numpy objects, in order (X, y)
+        """
+        
+        X_data = []
+        y_data = []
 
-        ## pe_sections
-        names = [n['name'] for n in obj['static']['pe_sections']]
+        # loading data from mongodb
+        limit_data = self.db.analysis.count()
+        db_data = self.db.analysis.find()
+        count = 0
+        for data_dict in db_data:
+            count += 1
+            progress(count, limit_data)
 
-        for section in pe_sections:
-            X.append(1 if section in names else 0)
+            obj = dict(data_dict)
 
-        ## pe_imports
-        for dll_name, funs in pe_imports:
-            gen = False
+            X = []
+            if 'static' in obj and 'pe_sections' in obj['static']\
+                    and 'pe_imports' in obj['static'] and 'virustotal' in obj:
 
-            for dll in obj['static']['pe_imports']:
-                if dll['dll'] == dll_name:
-                    gen = True
-                    X.append(1)
+                ## pe_sections
+                names = {}
+                for n in obj['static']['pe_sections']:
+                    names[n['name']] = [int(n['size_of_data'], 16), int(n['virtual_address'], 16),
+                                        n['entropy'], int(n['virtual_size'], 16)]
+
+                for section in self.pe_sections:
+                    if section in names:
+                        X.extend(names[section])
+                    else:
+                        X.extend([0, 0, 0, 0)
+
+                ## pe_imports
+                for dll_name, funs in self.pe_imports:
+                    gen = False
+
+                    for dll in obj['static']['pe_imports']:
+                        if dll['dll'].lower() == dll_name:
+                            gen = True
+                            
+                            imports_funs = {}
+                            for f in dll['imports']:
+                                imports_funs[f['name'].lower()] = int(f['address'], 16)
+
+                            for fun in funs:
+                                if fun in imports_funs:
+                                    X.append(imports_funs[fun])
+                                else:
+                                    X.append(0)
+
+                            break
                     
-                    imports_funs = [f['name'] for f in dll['imports']]
-                    for fun in funs:
-                        X.append(1 if fun in imports_funs else 0)
+                    if not gen:
+                        for fun in funs:
+                            X.append(0)
 
-                    break
-            
-            if not gen:
-                X.append(0)
-                for fun in funs:
-                    X.append(0)
+                X_data.append(np.array(X))
 
-        dataset.append((X, obj['label']))
+                # check y label with virus_total submission
+                if obj['virustotal']['positives'] / obj['virustotal']['total'] >= MIN_POSITIVES_RATE:
+                    y_data.append(1)
+                else:
+                    y_data.append(0)
 
-# output labeled features
-features = pe_sections
-for dll_name, funs in pe_imports:
-    features.append(dll_name)
-    for f in funs:
-        features.append(f)
-
-print()
-utils.save_data(features, 'features.pkl')
-
-print('\n')
-utils.save_data(dataset, 'dataset.pkl')
+       return (np.array(X_data), np.array(y_data)) 
